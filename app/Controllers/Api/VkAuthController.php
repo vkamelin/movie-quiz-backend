6c60249b3f986133f3c87b45fd7ffdddae9aac55
Vitaliy Kamelin <v.kamelin@gmail.com>
2026-02-01 13:08:25 +0500

    feat: Исправлена обработка данных VK API (main)
    Переименован ключ 'data' на 'payload'
    для корректного получения vk_user_id.

--- app/Controllers/Api/VkAuthController.php
+++ app/Controllers/Api/VkAuthController.php
@@ -23,9 +23,9 @@ class VkAuthController extends VkController
             return $this->error($response, 'Invalid signature', 401);
         }
 
-        // Парсим data, чтобы получить vk_user_id
-        parse_str($params['payload'], $dataParsed);
-        $userId = (int)($dataParsed['vk_user_id'] ?? 0);
+        // Парсим data, чтобы получить user_id
+        $data = $this->parsePayload($params['payload']);
+        $userId = (int)($data['user_id'] ?? 0);
 
         if (!$userId) {
             return $this->error($response, 'User ID not found in data', 400);
@@ -39,5 +39,17 @@ class VkAuthController extends VkController
         $token = JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256');
 
         return $this->success($response, ['token' => $token]);
+    }
+
+    private function parsePayload(string $payload): array
+    {
+        $data = [];
+        $payloadData = explode(';', $payload);
+        foreach ($payloadData as $item) {
+            $value = explode('=', $item);
+            $data[$value[0]] = $value[1];
+        }
+
+        return $data;
     }
 }
