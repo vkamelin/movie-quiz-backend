6fed8dc9a905653d4a3978d45b5782d1698c74f1
Vitaliy Kamelin <v.kamelin@gmail.com>
2026-02-01 21:28:13 +0500

    feat: Обновлено добавление фильмов (main)
    Изменения в `ImportMoviesCommand`:
    - Добавлена возможность указывать
      'page', 'limit', 'maxPages'
      при вызове команды.
    - Улучшена обработка ошибок API.
    - Переименован класс KinopoiskApi
      в PoiskKinoApi.
    - Убраны лишние поля из запроса
      к API.
    - Добавлено игнорирование
      документальных фильмов.
    - Добавлено игнорирование
      фильмов без названия.
    - Добавлена задержка между
      запросами к API.

--- app/Console/Commands/ImportMoviesCommand.php
+++ app/Console/Commands/ImportMoviesCommand.php
@@ -7,8 +7,9 @@ use PDO;
 use App\Console\Command;
 use App\Console\Kernel;
 use App\Helpers\Database;
-use App\Helpers\KinopoiskApi;
+use App\Helpers\PoiskKinoApi;
 use PDOException;
+use Throwable;
 
 class ImportMoviesCommand extends Command
 {
@@ -18,9 +19,25 @@ class ImportMoviesCommand extends Command
     public function handle(array $arguments, Kernel $kernel): int
     {
         $page = 1;
+        $limit = 250;
+        $maxPages = 1;
 
-        if (isset($arguments['page'])) {
-            $page = (int)$arguments['page'];
+        if (count($arguments) > 0) {
+            foreach ($arguments as $argument) {
+                $argumentArray = explode('=', $argument);
+
+                if ($argumentArray[0] == 'page') {
+                    $page = (int)$argumentArray[1];
+                }
+
+                if ($argumentArray[0] == 'limit') {
+                    $limit = (int)$argumentArray[1];
+                }
+
+                if ($argumentArray[0] == 'maxPages') {
+                    $maxPages = (int)$argumentArray[1];
+                }
+            }
         }
 
         $db = Database::getInstance();
@@ -28,82 +45,132 @@ class ImportMoviesCommand extends Command
         $genres = $db->query('SELECT id, name FROM genres')->fetchAll(PDO::FETCH_KEY_PAIR);
         $countries = $db->query('SELECT id, name FROM countries')->fetchAll(PDO::FETCH_KEY_PAIR);
 
-        $api = new KinopoiskApi($_ENV['MOVIES_API_KEY']);
+        $api = new PoiskKinoApi($_ENV['MOVIES_API_KEY']);
 
-        $params = [
-            'order' => 'RATING',
-            'type' => 'FILM',
-            'ratingFrom' => 6,
-            'ratingTo' => 10,
-            'yearFrom' => 1000,
-            'yearTo' => 3000,
-            'page' => $page,
-        ];
+        for ($i = 0; $i < $maxPages; $i++) {
+            $currentPage = $page + $i;
 
-        $moviesResult = $api->searchFilms($params);
+            try {
+                $moviesResult = $api->movies()
+                    ->withType('movie')
+                    ->withPage($currentPage)
+                    ->withLimit($limit)
+                    ->withNotNullFields('rating.kp')
+                    ->withSortFields('rating.kp')
+                    ->withSortTypes('-1')
+                    ->execute();
+            } catch (Throwable $e) {
+                Logger::error($e->getMessage());
+                echo $e->getMessage() . PHP_EOL;
+                echo $e->getTraceAsString() . PHP_EOL;
+                return 0;
+            }
 
-        if ($moviesResult) {
-            if ($moviesResult['total'] > 0) {
-                foreach ($moviesResult['items'] as $movie) {
-                    $movieGenres = [];
-                    $movieCountries = [];
+            if ($moviesResult) {
+                if ($moviesResult['total'] > 0) {
+                    foreach ($moviesResult['docs'] as $movie) {
+                        $movieGenres = [];
+                        $movieCountries = [];
 
-                    // genres
-                    if (!empty($movie['genres'])) {
-                        foreach ($movie['genres'] as $genre) {
-                            if (!in_array($genre['genre'], $genres)) {
-                                $genreId = $this->addGenre($genre['genre']);
-                                $genres[$genreId] = $genre['genre'];
+                        // genres
+                        if (!empty($movie['genres'])) {
+                            $skipMovie = false;
 
-                                $movieGenres[] = $genreId;
-                            } else {
-                                $movieGenres[] = array_search($genre['genre'], $genres);
+                            foreach ($movie['genres'] as $genre) {
+                                // skip movies with genre "документальный"
+                                if ($genre['name'] === 'документальный') {
+                                    $skipMovie = true;
+                                    break;
+                                }
+
+                                if (!in_array($genre['name'], $genres)) {
+                                    $genreId = $this->addGenre($genre['name']);
+                                    $genres[$genreId] = $genre['name'];
+
+                                    $movieGenres[] = $genreId;
+                                } else {
+                                    $movieGenres[] = array_search($genre['name'], $genres);
+                                }
+                            }
+
+                            if ($skipMovie) {
+                                continue;
                             }
                         }
-                    }
 
-                    // countries
-                    if (!empty($movie['countries'])) {
-                        foreach ($movie['countries'] as $country) {
-                            if (!in_array($country['country'], $countries)) {
-                                $countryId = $this->addCountry($country['country']);
-                                $countries[$countryId] = $country['country'];
+                        // countries
+                        if (!empty($movie['countries'])) {
+                            foreach ($movie['countries'] as $country) {
+                                if (!in_array($country['name'], $countries)) {
+                                    $countryId = $this->addCountry($country['name']);
+                                    $countries[$countryId] = $country['name'];
 
-                                $movieCountries[] = $countryId;
-                            } else {
-                                $movieCountries[] = array_search($country['country'], $countries);
+                                    $movieCountries[] = $countryId;
+                                } else {
+                                    $movieCountries[] = array_search($country['name'], $countries);
+                                }
                             }
                         }
-                    }
 
-                    $db->beginTransaction();
+                        if (empty($movie['name'])) {
+                            // skip movies without title
+                            continue;
+                        }
 
-                    try {
-                        $movieQuery = $db->prepare("INSERT INTO `movies` (kinopoisk_id, imdb_id, title, title_original, poster_url, poster_url_preview, rating_kinopoisk, rating_imdb, year) VALUES (:kinopoisk_id, :imdb_id, :title, :title_original, :poster_url, :poster_url_preview, :rating_kinopoisk, :rating_imdb, :year)");
+                        $db->beginTransaction();
 
-                        $movieQuery->execute([
-                            'kinopoisk_id' => $movie['kinopoiskId'],
-                            'imdb_id' => $movie['imdbId'],
-                            'title' => $movie['nameRu'],
-                            'title_original' => $movie['nameOriginal'],
-                            'poster_url' => $movie['posterUrl'],
-                            'poster_url_preview' => $movie['posterUrlPreview'],
-                            'rating_kinopoisk' => $movie['ratingKinopoisk'],
-                            'rating_imdb' => $movie['ratingImdb'],
-                            'year' => $movie['year'],
-                        ]);
-                    } catch (PDOException $e) {
-                        $db->rollBack();
-                        Logger::error($e->getMessage());
-                        continue;
+                        try {
+                            $movieQuery = $db->prepare("INSERT INTO `movies` (`kinopoisk_id`, `title`, `title_original`, `poster_url`, `poster_url_preview`, `rating_kinopoisk`, `rating_imdb`, `year`, `description`) VALUES (:kinopoisk_id, :title, :title_original, :poster_url, :poster_url_preview, :rating_kinopoisk, :rating_imdb, :year, :description)");
+
+                            $movieQuery->execute([
+                                'kinopoisk_id' => $movie['id'],
+                                'title' => $movie['name'],
+                                'title_original' => $movie['alternativeName'],
+                                'poster_url' => !empty($movie['poster']['url']) ? $movie['poster']['url'] : '',
+                                'poster_url_preview' => !empty($movie['poster']['previewUrl']) ? $movie['poster']['previewUrl'] : null,
+                                'rating_kinopoisk' => !empty($movie['rating']['kp']) ? $movie['rating']['kp'] : null,
+                                'rating_imdb' => !empty($movie['rating']['imdb_id']) ? $movie['rating']['imdb_id'] : null,
+                                'year' => $movie['year'] ?? null,
+                                'description' => !empty($movie['description']) ? $movie['description'] : '',
+                            ]);
+
+                            $movieId = $db->lastInsertId();
+
+                            if (!empty($movieGenres)) {
+                                foreach ($movieGenres as $genreId) {
+                                    $genreQuery = $db->prepare('INSERT INTO movies_genres (movie_id, genre_id) VALUES (:movie_id, :genre_id)');
+                                    $genreQuery->execute([
+                                        'movie_id' => $movieId,
+                                        'genre_id' => $genreId
+                                    ]);
+                                }
+                            }
+
+                            if (!empty($movieCountries)) {
+                                foreach ($movieCountries as $countryId) {
+                                    $countryQuery = $db->prepare('INSERT INTO movies_countries (movie_id, country_id) VALUES (:movie_id, :country_id)');
+                                    $countryQuery->execute([
+                                        'movie_id' => $movieId,
+                                        'country_id' => $countryId
+                                    ]);
+                                }
+                            }
+
+                            $db->commit();
+                        } catch (PDOException $e) {
+                            $db->rollBack();
+                            Logger::error($e->getMessage());
+                            continue;
+                        }
                     }
                 }
             }
 
-            if ($filmsResult['totalPages'] == 0) {
-                return 0;
-            }
+            echo "Страница {$currentPage} обработана" . PHP_EOL;
+            sleep(5);
         }
+
+        echo "Импорт завершен" . PHP_EOL;
 
         return 0;
     }
