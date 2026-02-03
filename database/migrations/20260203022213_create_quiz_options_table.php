eea692628e6de87494368e8509976a1fb86417cf
Vitaliy Kamelin <v.kamelin@gmail.com>
2026-02-03 07:25:00 +0500

    feat: Добавить индекс к таблице quiz_options (main)
    Добавляет индекс `idx_quiz_options_movie_id` к таблице `quiz_options`.

--- database/migrations/20260203022213_create_quiz_options_table.php
+++ database/migrations/20260203022213_create_quiz_options_table.php
@@ -6,7 +6,7 @@ use Phinx\Migration\AbstractMigration;
 
 final class CreateQuizOptionsTable extends AbstractMigration
 {
-    public function ip(): void
+    public function up(): void
     {
         $this->execute("CREATE TABLE IF NOT EXISTS `quiz_options` (
             `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -15,5 +15,12 @@ final class CreateQuizOptionsTable extends AbstractMigration
             `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
         );");
+
+        $this->execute("CREATE INDEX `idx_quiz_options_movie_id` ON `quiz_options` (`movie_id`);");
+    }
+
+    public function down(): void
+    {
+        $this->execute("DROP TABLE IF EXISTS `quiz_options`;");
     }
 }
